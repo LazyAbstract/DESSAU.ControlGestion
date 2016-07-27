926c76595542592d91c2c3b5356bcdc2f857a0f1
Nicolás Giannetti <juannetti@gmail.com>
2016-07-26 20:23:30 -0400

    ADDED: style startbootstrap

---
+++ DESSAU.ControlGestion.Web/Controllers/ReporteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DESSAU.ControlGestion.Web.Controllers
+{
+    public class ReporteController : BaseController
+    {
+        // GET: Reporte
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
