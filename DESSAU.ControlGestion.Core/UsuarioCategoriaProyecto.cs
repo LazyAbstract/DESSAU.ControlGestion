8163c16f179f1527a5334b669ca77ba1cfa65c99
Nicolás Giannetti <juannetti@gmail.com>
2016-07-29 17:37:58 -0400

    ADDED: nominas

---
+++ DESSAU.ControlGestion.Core/UsuarioCategoriaProyecto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESSAU.ControlGestion.Core
+{
+    public partial class UsuarioCategoriaProyecto
+    {
+        public bool PlanificacionOk
+        {
+            get
+            {
+                bool response = true;
+                EstadoPlanificacionDeclaracion epd = this.EstadoPlanificacionDeclaracions.OrderByDescending(x => x.Ano).ThenBy(x => x.Mes).FirstOrDefault();
+                if (epd != null) response = epd.PlanificacionOk;
+                return response;
+            }
+        }
+
+        public bool DeclaracionOk
+        {
+            get
+            {
+                bool response = true;
+                EstadoPlanificacionDeclaracion epd = this.EstadoPlanificacionDeclaracions.OrderByDescending(x => x.Ano).ThenBy(x => x.Mes).FirstOrDefault();
+                if (epd != null) response = epd.DeclaracionOk;
+                return response;
+            }
+        }
+    }
+}
---
+++ DESSAU.ControlGestion.Web/Controllers/NominaController.cs
@@ -0,0 +1,32 @@
+﻿using DESSAU.ControlGestion.Core;
+using DESSAU.ControlGestion.Web.Models.NominaModels;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DESSAU.ControlGestion.Web.Controllers
+{
+    public class NominaController : BaseController
+    {
+        // GET: Nomina
+        public ActionResult Index()
+        {
+            return RedirectToAction("VerNomina");
+        }
+
+        [HttpGet]
+        public ActionResult VerNomina(int? pagina, VerNominaFormModel Form)
+        {
+            VerNominaViewModel model = new VerNominaViewModel();
+            IQueryable<UsuarioCategoriaProyecto> Nominas = db.UsuarioCategoriaProyectos
+                    .OrderBy(x => x.Categoria.Nombre).ThenBy(x => x.Usuario.ApellidoPaterno);
+            if (Form.IdProyecto.HasValue) Nominas = db.UsuarioCategoriaProyectos
+                    .Where(x => x.IdProyecto == Form.IdProyecto);
+            model.Nominas = Nominas.ToPagedList(pagina ?? 1, 10);
+            return View(model);
+        }
+    }
+}
