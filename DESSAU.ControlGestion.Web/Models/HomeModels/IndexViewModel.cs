5e33f06f15b70395bc39106f63161f32568fbc30
Nicolás Giannetti <juannetti@gmail.com>
2016-08-03 02:32:47 -0400

    ADDED: calendarios index

---
+++ DESSAU.ControlGestion.Web/Models/HomeModels/IndexViewModel.cs
@@ -0,0 +1,43 @@
+﻿using DESSAU.ControlGestion.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DESSAU.ControlGestion.Web.Models.HomeModels
+{
+    public class IndexViewModel
+    {
+        public DESSAUControlGestionDataContext db = new DESSAUControlGestionDataContext()
+            .WithConnectionStringFromConfiguration();
+        public IEnumerable<object> DiasPendientesPlanificacion { get; set; }
+        public IEnumerable<object> DiasPendientesDeclaracion { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public IndexViewModel()
+        {
+
+        }
+
+        public IndexViewModel(int IdUsuario) : this()
+        {
+            var buffer = db.sp_GetDiasPendientesPlanificacionByIdUsuario(IdUsuario);
+            if (buffer != null)
+                DiasPendientesPlanificacion = buffer.Select(x => new
+                {
+                    date = x.Fecha.HasValue ? x.Fecha.Value.ToString("yyyy-MM-dd")
+                        : String.Empty,
+                    title = "Planificación Pendiente"
+                }).ToList();
+
+            var buffer2 = db.sp_GetDiasPendientesDeclaracionByIdUsuario(IdUsuario);
+            if (buffer2 != null)
+                DiasPendientesDeclaracion = buffer2.Select(x => new
+                {
+                    date = x.Fecha.HasValue ? x.Fecha.Value.ToString("yyyy-MM-dd")
+                        : String.Empty,
+                    title = "Declaración Pendiente"
+                }).ToList();
+        }
+    }
+}
