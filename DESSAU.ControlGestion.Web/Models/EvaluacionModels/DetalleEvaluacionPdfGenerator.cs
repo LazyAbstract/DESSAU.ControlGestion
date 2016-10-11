daca8d52683f42c5535bd6275f054d70414ac1a2
xruiz <xruizv@gmail.com>
2016-10-11 08:39:12 -0300

    intermedio

---
+++ DESSAU.ControlGestion.Web/Models/EvaluacionModels/DetalleEvaluacionPdfGenerator.cs
@@ -0,0 +1,88 @@
+﻿using DESSAU.ControlGestion.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace DESSAU.ControlGestion.Web.Models.EvaluacionModels
+{
+    public class DetalleEvaluacionPdfGenerator
+    {
+        private Evaluacion _evaluacion { get; set; }
+
+        public DetalleEvaluacionPdfGenerator(Evaluacion evaluacion)
+        {
+            _evaluacion = evaluacion;
+        }
+
+
+
+        public byte[] getDetalleEvaluacionPdf(DESSAUControlGestionDataContext db, Image imagenLogo) {
+            byte[] buffer = null;
+            using (MemoryStream ms = new MemoryStream())
+            using (Document document = new Document(PageSize.LETTER, 30f, 30f, 40f, 30f))
+            using (PdfWriter writer = PdfWriter.GetInstance(document, ms))
+            {
+                LogoDessauEncabezadoPdfEventHandler e = new LogoDessauEncabezadoPdfEventHandler()
+                {
+                    ImageHeader = imagenLogo,
+                    Titulo = "Evaluación de desempeño"
+                };
+                writer.PageEvent = e;
+                document.Open();
+                generarDetalleEvaluacionPdf(document, db);
+                document.Close();
+                buffer = ms.ToArray();
+                ms.Close();
+            }
+            return buffer;
+        }
+
+
+        private Font Titulo = FontFactory.GetFont("Arial", 14, Font.BOLD, BaseColor.BLACK);
+        private Font Normal = FontFactory.GetFont("Arial", 12, Font.NORMAL, BaseColor.DARK_GRAY);
+        private Font Cabecera = FontFactory.GetFont("Arial", 12, Font.BOLD, BaseColor.DARK_GRAY);
+        public void generarDetalleEvaluacionPdf(Document doc, DESSAUControlGestionDataContext db)
+        {
+            //            I.- IDENTIFICACIÓN TRABAJADOR EVALUADO
+            //NOMBRE: YANITZA ARAYA
+            //SERVICIO: Control Documental
+            //DIRECCION: DFE
+            //CONTRATO:	Servicio de apoyo para proyectos especiales GPRO
+            doc.Add(new Paragraph("\n\n", FontFactory.GetFont("Arial", 6)));
+            doc.Add(new Paragraph("I.- Identificación de trabajador evaluado", Titulo));
+            doc.Add(new Paragraph("\n\n", FontFactory.GetFont("Arial", 6)));
+            PdfPTable identificacion = new PdfPTable(new float[] { 1, 5 }) {  WidthPercentage=100f};
+            identificacion.AddCell(getCabeceraCell("Nombre"));
+            identificacion.AddCell(getNormalCell(_evaluacion.UsuarioCategoriaProyecto.Usuario.NombreCompleto));
+            identificacion.AddCell(getCabeceraCell("Sevicio"));
+            identificacion.AddCell(getNormalCell(_evaluacion.UsuarioCategoriaProyecto.Categoria.Nombre));
+            identificacion.AddCell(getCabeceraCell("Dirección"));
+            identificacion.AddCell(getNormalCell(_evaluacion.UsuarioCategoriaProyecto.Proyecto.Usuario.NombreCompleto));
+            identificacion.AddCell(getCabeceraCell("Contrato"));
+            identificacion.AddCell(getNormalCell(_evaluacion.UsuarioCategoriaProyecto.Proyecto.Contrato.Nombre));
+            doc.Add(identificacion);
+        }
+
+        private PdfPCell getCabeceraCell(string frase) {
+            return new PdfPCell(new Phrase(frase, Cabecera))
+            {
+                VerticalAlignment = Rectangle.ALIGN_MIDDLE,
+                BackgroundColor = BaseColor.LIGHT_GRAY,
+                Border = Rectangle.NO_BORDER
+            };
+        }
+
+        private PdfPCell getNormalCell(string frase)
+        {
+            return new PdfPCell(new Phrase(frase, Normal))
+            {
+                VerticalAlignment = Rectangle.ALIGN_MIDDLE,
+                BorderColor = BaseColor.LIGHT_GRAY
+            };
+        }
+    }
+}
---
+++ DESSAU.ControlGestion.Web/Models/LogoDessauEncabezadoPdfEventHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace DESSAU.ControlGestion.Web.Models
+{
+    public class LogoDessauEncabezadoPdfEventHandler : PdfPageEventHelper
+    {
+        public Image ImageHeader { get; set; }
+        public string Titulo { get; set; }
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            // cell height 
+            float cellHeight = document.TopMargin;
+            // PDF document size      
+            Rectangle page = document.PageSize;
+            PdfPTable head = new PdfPTable(new float[] {1,3,1});
+            head.TotalWidth = page.Width-document.LeftMargin-document.RightMargin;
+
+            PdfPCell c = new PdfPCell(ImageHeader);
+            c.HorizontalAlignment = Element.ALIGN_LEFT;
+            c.VerticalAlignment = Element.ALIGN_BOTTOM;
+            c.FixedHeight = cellHeight;
+            c.Border = PdfPCell.NO_BORDER;
+            head.AddCell(c);
+
+            c = new PdfPCell(new Phrase(Titulo, FontFactory.GetFont("Arial", 16, Font.BOLD,BaseColor.BLACK)));
+            c.HorizontalAlignment = Element.ALIGN_CENTER;
+            c.VerticalAlignment = Element.ALIGN_BOTTOM;
+            c.FixedHeight = cellHeight;
+            c.Border = PdfPCell.NO_BORDER;
+            head.AddCell(c);
+            
+            c = new PdfPCell(new Phrase(
+              DateTime.UtcNow.ToString("dd/MM/yyyy"),
+              FontFactory.GetFont("Arial", 12, Font.NORMAL, BaseColor.BLACK)
+            ));
+            c.Border = PdfPCell.NO_BORDER;
+            c.HorizontalAlignment = Element.ALIGN_RIGHT;
+            c.VerticalAlignment = Element.ALIGN_BOTTOM;
+            c.FixedHeight = cellHeight;
+            head.AddCell(c);
+
+            // since the table header is implemented using a PdfPTable, we call
+            // WriteSelectedRows(), which requires absolute positions!
+            head.WriteSelectedRows(
+              0, -1,  // first/last row; -1 flags all write all rows
+              document.LeftMargin,      // left offset
+              // ** bottom** yPos of the table
+              page.Height - cellHeight + head.TotalHeight,
+              writer.DirectContent
+            );
+        }
+    }
+}
